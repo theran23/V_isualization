caf54b55507a152d5ba9579690bca0eced35a238
Seran Kim <49436143+theran23@users.noreply.github.com>
2022-01-03 11:05:28 +0900

    .

--- sorting/views.py
+++ sorting/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 class SortingPageView(TemplateView):
 
-    template_name = 'sorting/templates/sorting.html'
+    template_name = 'home/templates/sorting.html'
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(request, *args, **kwargs)
@@ -16,4 +16,5 @@ class SortingPageView(TemplateView):
 
     def get_context_data(self, request, *args, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['array'] = [1,2,5,4,3,11,7,19,20]
         return context
