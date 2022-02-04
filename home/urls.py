b63da357654a4b3f411e5e154d449e67b329c45e
Seran Kim <49436143+theran23@users.noreply.github.com>
2022-02-04 15:06:07 +0900

    1. url 합침 2. sorting 구현

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # prototype
     # url(r'^$', views.index, name='index'),
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^sorting/$', SortingPageView.as_view(), name='sorting'),
+    url(r'^search/$', SearchPageView.as_view(), name='search'),
 ]
--- v_isualization/urls.py
+++ v_isualization/urls.py
@@ -20,6 +20,5 @@ from django.conf.urls import url, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'', include('home.urls')),
-    url('sorting/', include('sorting.urls')),
 
 ]
